fe207a134d8eb61c8731b4ecf2462187f8ecac05
KushchO <lvioleg@gmail.com>
2019-10-31 10:13:55 +0300

    Поправил скрипт popup

--- src/blocks/components/request-form/request-popup.js
+++ src/blocks/components/request-form/request-popup.js
@@ -3,23 +3,22 @@ const overlay = document.querySelector('.popup_overlay');
 const requestPopup = document.querySelector('.request-form__popup');
 if (requestPopup) {
 	const close = requestPopup.querySelector('.close-element');
-}
+	if (requestButton && overlay && requestPopup && close) {
+		requestButton.addEventListener('click', function() {
+			overlay.style.display = 'block';
+			requestPopup.style.display = 'block';
+			requestPopup.style.opacity = '1';
+		});
 
-if (requestButton && overlay && requestPopup) {
-	requestButton.addEventListener('click', function() {
-		overlay.style.display = 'block';
-		requestPopup.style.display = 'block';
-		requestPopup.style.opacity = '1';
-	});
-
-	close.addEventListener('click', function() {
-		overlay.style.display = 'none';
-		requestPopup.style.display = 'none';
-		requestPopup.style.opacity = '0';
-	});
-	overlay.addEventListener('click', function() {
-		overlay.style.display = 'none';
-		requestPopup.style.display = 'none';
-		requestPopup.style.opacity = '0';
-	});
+		close.addEventListener('click', function() {
+			overlay.style.display = 'none';
+			requestPopup.style.display = 'none';
+			requestPopup.style.opacity = '0';
+		});
+		overlay.addEventListener('click', function() {
+			overlay.style.display = 'none';
+			requestPopup.style.display = 'none';
+			requestPopup.style.opacity = '0';
+		});
+	}
 }
